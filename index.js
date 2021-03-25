82341b4f1d39f362e825135417b6d2d28ac055e9
iLLMannerSevs <shadowsuspect707@gmail.com>
2021-03-25 22:49:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+function SpawnObject( BuildingName, pos, yaw )
+{
+var cords = pos.split(" ")
+var a = yaw.split(" ")
+var x = cords[0]
+var y = cords[1]
+var z = cords[2]
+var a = a[0]
+
+console.log("	<event name=" + '"' + BuildingName + '"'+ ">")
+console.log("		<pos x=" + '"' + x + '"', "y=" + '"' + y + '"', "z=" + '"' + z + '"', "a=" + '"' + a + '"' + " />")
+console.log("	</event>")
+}
+
+/*Paste Co ordinates below to format them for the cfgeventspawns.xml file*/
+//Spawn helper function
+
+// Paste anything below this line into the bottom of your 'void main()' function
+// Created Objects
+SpawnObject("Land_Mil_Fortified_Nest_Big", "2109.810059 110.602997 3536.199951", "-165.000000 0.000000 0.000000");
+SpawnObject("Land_Mil_Fortified_Nest_Small", "2125.060059 110.750999 3519.709961", "-110.000000 -5.677680 -1.581360");
+SpawnObject("Land_Mil_Fortified_Nest_Watchtower", "2114.239990 111.325996 3497.719971", "-115.000000 0.000000 0.000000");
+SpawnObject("Land_Mil_Fortified_Nest_Small", "2128.590088 109.101997 3502.489990", "-24.999985 -2.966890 5.870550");
+SpawnObject("Land_Mil_Fortified_Nest_Small", "2109.959961 110.102997 3490.840088", "-27.000000 1.956650 1.657850");
+SpawnObject("Land_Container_1Moh", "2068.558105 111.333557 3535.615479", "-108.000008 0.221567 -0.553974");
+SpawnObject("Land_Container_1Moh", "2038.215576 111.472824 3515.026611", "-18.000032 0.638666 1.719478");
+SpawnObject("Land_Container_1Moh", "2087.125000 109.654861 3472.156982", "-116.999985 -1.470107 -7.594395");
+SpawnObject("Land_Container_1Mo", "2092.666504 109.741341 3474.587158", "-116.999947 -1.434736 -6.855318");
+SpawnObject("Land_Container_1Mo", "2087.745361 105.038658 3408.864258", "-71.999969 22.781115 2.479258");
+SpawnObject("Land_Container_1Mo", "2092.313965 102.514320 3407.323486", "-72.000000 23.339479 5.077985");
+SpawnObject("Land_Container_1Mo", "2096.716064 99.010269 3406.482666", "98.999901 -26.811317 -2.356775");
+SpawnObject("Land_Container_1Mo", "2130.801514 94.896683 3409.695801", "-107.999817 -1.144182 -4.516130");
+SpawnObject("Land_Container_1Mo", "2142.309814 95.195602 3414.673096", "63.000000 3.532076 14.326445");
+SpawnObject("Land_Container_1Mo", "2136.740234 94.903694 3411.876221", "-116.999916 -0.096267 -3.718096");
+SpawnObject("Land_Container_1Mo", "2168.367188 96.416389 3414.503906", "-90.000008 2.325016 -8.037732");
+SpawnObject("Land_Container_1Mo", "2061.503418 98.695717 3317.948486", "-9.000000 5.100904 9.180490");
+SpawnObject("Land_Container_1Mo", "2062.352295 97.526443 3312.232178", "-9.000000 15.703310 2.236055");
+SpawnObject("Land_Tank_Medium_Stairs", "2078.360107 109.848999 3411.889893", "-164.500015 1.934090 -25.276100");
+SpawnObject("Land_Mil_Tent_Big3", "2095.629883 110.712997 3522.810059", "-114.999962 -0.023612 -0.072671");
+SpawnObject("Land_Mil_Tent_Big4", "2116.790039 110.857002 3514.669922", "-24.999987 0.000000 0.000000");
+SpawnObject("Land_Tisy_RadarPlatform_Top", "2077.570068 119.473999 3500.239990", "-22.999968 0.305659 -0.241501");
+SpawnObject("Land_Tisy_Barracks", "2212.929443 105.400879 3274.140381", "-135.000137 -0.000000 -0.000000");
+SpawnObject("Land_Mil_Tent_Big1_2", "1976.433105 104.964256 3211.122070", "0.000000 5.226525 -0.608576");
+SpawnObject("Land_Mil_Airfield_HQ", "2288.290039 137.330002 3695.800049", "49.000198 0.500000 0.000000");
+SpawnObject("Land_Mil_CamoNet_Roof_east", "2301.098389 134.073822 3679.966553", "-134.999954 -6.163486 -3.218575");
+SpawnObject("Land_Roadblock_WoodenCrate", "2171.885742 102.430099 3353.866943", "0.000000 0.076408 -0.381989");
+SpawnObject("Land_Roadblock_WoodenCrate", "2164.976074 102.431892 3361.475098", "0.000000 0.229157 -0.458334");
+SpawnObject("Land_Tisy_Base_cooler", "2133.469971 94.756798 3368.610107", "-90.000000 2.135850 -0.687012");
+SpawnObject("Land_Mil_Fortified_Nest_Small", "2099.330078 92.066803 3296.520020", "5.000102 -0.353314 -0.210683");
+SpawnObject("Land_Mil_Fortified_Nest_Small", "2069.419922 93.692200 3289.610107", "98.999901 -8.794140 5.967760");
+SpawnObject("Land_Mil_Fortified_Nest_Watchtower", "2094.270020 94.337502 3287.179932", "92.500000 -1.450550 -0.916381");
+SpawnObject("Land_Mil_Fortified_Nest_Big", "2109.399902 91.492203 3297.530029", "0.000000 0.075660 6.692140");
